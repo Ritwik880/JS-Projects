5664f6ed242a652b58abd5ad4141d8095255fd61
Ritwik880 <rittiksinha12@gmail.com>
2021-07-21 09:11:34 +0530

    modilied

---
+++ Car-Animated/js/script.js
@@ -0,0 +1,4 @@
+var audio = document.createElement('audio');
+audio.setAttribute('src', '../media/sound.mp3'); 
+audio.loop=true;
+// audio.play();
--- breakout/README.md
+++
@@ -1,4 +0,0 @@
-# Game Demo
-
-![game-demo](https://media.giphy.com/media/HdFCKMsIDCkYSR1oB5/giphy.gif)
-
